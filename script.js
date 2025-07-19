452e5370ae6447811bba7e5f02fbeb6e41e0f991
MIKAEL <Mikaelpebreta896@gmail.com>
2025-07-19 10:40:16 +0700

    Initialize Firebase app, auth, and firestore

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-// Register Service Worker
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCmFplJjCa0Ry6JEUoqyzBGfxKTMp55z8s",
+  authDomain: "pendudukapp-8d297.firebaseapp.com",
+  projectId: "pendudukapp-8d297",
+  storageBucket: "pendudukapp-8d297.firebasestorage.app",
+  messagingSenderId: "521712480220",
+// Inisialisasi Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth(); // Untuk Autentikasi
+const db = firebase.firestore(); // Untuk Firestore Database
+
+console.log("Firebase initialized:", app);
+console.log("Firebase Auth:", auth);
+console.log("Firestore DB:", db);  appId: "1:521712480220:web:d7aa47c6117d483f926919"
+};// Register Service Worker
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('/sw.js')
